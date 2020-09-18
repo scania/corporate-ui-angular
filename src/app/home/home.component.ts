5ee55f32704b1a4c3ff177da02ab8598fc9fa1f2
Marcus Mena <marcus.mena.pacheco@scania.com>
2020-09-18 11:19:19 +0200

    update packages readme and example
    - Update packages versions
    - Updated readme with more links
    - Added link inside the example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
+  <div class="container">
     <h1>Home</h1>
-    <p>Some text goes here</p>
+    <h3>Visit Scania Digtal Design System at <a href='https://digitaldesign.scania.com'>digitaldesign.scania.com</a><c-icon name='scania-external_link'></c-icon></h3>
+    <p>Developing in our design system visit our <a href='https://github.com/scania/corporate-ui'>github</a><c-icon name='scania-external_link'></c-icon></p>
+  </div>
   `
 })
 export class HomeComponent implements OnInit {
